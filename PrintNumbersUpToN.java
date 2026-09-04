7cdddf15993cbc7ee072a41157f17b5666611c14
sivavignesh-960 <sp8224@srmist.edu.in>
2026-09-04 20:49:01 +0530

    Print Numbers 1 to N

---
+++ PrintNumbersUpToN.java
@@ -0,0 +1,12 @@
+public class PrintNumbersUpToN {
+
+    static void printNumbersUpToN(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printNumbersUpToN(5);
+    }
+}
