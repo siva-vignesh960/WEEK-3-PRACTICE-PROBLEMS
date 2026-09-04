4889db81dc80f91ac2f5ef06b4b610b6fb074301
sivavignesh-960 <sp8224@srmist.edu.in>
2026-09-04 20:52:48 +0530

    Sum of Natural Numbers

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,18 @@
+public class SumOfNaturalNumbers {
+
+    static void sumOfNaturalNumbers(int n) {
+        int i = 1;
+        int sum = 0;
+
+        while (i <= n) {
+            sum = sum + i;
+            i++;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " = " + sum);
+    }
+
+    public static void main(String[] args) {
+        sumOfNaturalNumbers(5);
+    }
+}
