7b71e37db955324b7ac7ecc6fd3837805394ad41
sivavignesh-960 <sp8224@srmist.edu.in>
2026-09-04 21:00:47 +0530

    SkipMultiplesOfThree

---
+++ SkipMultiplesOfThree.java
@@ -0,0 +1,18 @@
+public class SkipMultiplesOfThree {
+
+    static void printSkippingMultiplesOfThree() {
+
+        for (int i = 1; i <= 20; i++) {
+
+            if (i % 3 == 0) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        printSkippingMultiplesOfThree();
+    }
+}
